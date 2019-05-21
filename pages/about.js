9b3a229fd69abe2b1e36f4a54c941035d42dc1be
andy <huayu.cao@pintec.com>
2019-05-21 15:54:09 +0800

    demo 1

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Layout from '../components/MyLayout';
+import {Component} from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <Layout>
+                <p>this is about</p>
+            </Layout>
+        );
+    }
+}
+
+export default About;
---
+++ pages/test.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TestPage extends Component {
+    render() {
+        return (
+            <div>
+                this is test
+            </div>
+        );
+    }
+}
+
+export default TestPage;
